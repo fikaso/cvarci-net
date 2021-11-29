a515f218d8a5e6b2a467bdd0b3094eb991630d29
fikaso <vranjes.filip@gmail.com>
2021-11-29 08:21:30 +0100

    Minor fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,10 @@ function Contact() {
     <div className="bg-gray  my-10 py-24">
       <div className="md:max-w-[1440px] mx-auto flex flex-col xl:flex-row text-center items-center justify-between space-y-5 xl-space-y-0 px-5 xl:px-20">
         <div>
-          <h2>Želite biti naš brand partner ?</h2>
+          <h2>
+            Želite biti naš brand{" "}
+            <span className="whitespace-nowrap">partner ?</span>{" "}
+          </h2>
           <p>
             Pošaljite nam vaš broj i kontaktirat ćemo Vas u najkraćem mogućem
             roku
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,10 @@
 function Location() {
   return (
     <div className="flex flex-col xl:flex-row space-y-5 xl-space-y-0 items-center justify-between text-center sm:text-left">
-      <h2 className="px-14">Gdje se nalaze naši čvarkomati ?</h2>
+      <h2 className="px-14">
+        Gdje se nalaze naši{" "}
+        <span className="whitespace-nowrap">čvarkomati ?</span>{" "}
+      </h2>
 
       <img src="./images/location.svg" alt="" />
     </div>
