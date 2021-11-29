49b8adaa507f8853b7ea93b1dd7b29ec4780bef0
fikaso <vranjes.filip@gmail.com>
2021-11-29 08:09:06 +0100

    Added intagram block to grid

--- src/components/InstagramBlock.js
+++ src/components/InstagramBlock.js
@@ -5,27 +5,29 @@ function InstagramBlock() {
         <h2>#čvarcinet</h2>
         <h3>na instagramu</h3>
       </div>
-      <div className="flex flex-col xl:flex-row justify-between items-center mt-5 space-y-5">
-        <img
-          className="max-h-[18.68rem] max-w-[18.5rem]"
-          src="./images/instagram_2.png"
-          alt=""
-        />
-        <img
-          className="max-h-[18.68rem] max-w-[18.5rem]"
-          src="./images/instagram_1.png"
-          alt=""
-        />
-        <img
-          className="max-h-[18.68rem] max-w-[18.5rem]"
-          src="./images/instagram_3.png"
-          alt=""
-        />
-        <img
-          className="max-h-[18.68rem] max-w-[18.5rem]"
-          src="./images/instagram_4.png"
-          alt=""
-        />
+      <div className="flex items-center justify-center">
+        <div className="container-grid xl:gap-x-16 mt-5">
+          <img
+            className="col-span-12 md:col-span-6 xl:col-span-3 max-h-[18.68rem] max-w-[18.5rem]"
+            src="./images/instagram_2.png"
+            alt=""
+          />
+          <img
+            className="col-span-12 md:col-span-6 xl:col-span-3 max-h-[18.68rem] max-w-[18.5rem]"
+            src="./images/instagram_1.png"
+            alt=""
+          />
+          <img
+            className="col-span-12 md:col-span-6 xl:col-span-3 max-h-[18.68rem] max-w-[18.5rem]"
+            src="./images/instagram_3.png"
+            alt=""
+          />
+          <img
+            className="col-span-12 md:col-span-6 xl:col-span-3 max-h-[18.68rem] max-w-[18.5rem]"
+            src="./images/instagram_4.png"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
